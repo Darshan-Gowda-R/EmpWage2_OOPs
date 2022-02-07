ab56d5be05fb15c2b9089eadb68ab54b6d738513
Darshan Gowda R <darshangowdar@pop-os.localdomain>
2022-02-07 20:49:39 +0530

    Refactor to have list of multiple companies to manage Employee Wage .

---
+++ Emp_wag/EmpUC12.java
@@ -0,0 +1,125 @@
+
+import java.util.*;
+
+public class EmpUC12{
+
+
+	public static void main(String[] args){
+
+	
+	Scanner sc = new Scanner(System.in);
+	     
+	System.out.println("Enter the number of company data to be calculated : ");
+	int num = sc.nextInt();
+	
+	ArrayList <Compute> arr = new ArrayList<Compute>(num);
+	
+	for(int i = 0 ; i < num ; i++ ){
+	
+		System.out.println("Enter the company name :");
+		
+		String name = sc.next();
+		
+		System.out.println("Enter Wage Pre Hour , Working Days and Total Working Hour");
+	
+		int wage = sc.nextInt();
+		int days = sc.nextInt();
+		int hour = sc.nextInt();
+	
+		Compute data = new Compute( name,wage,days,hour);
+		data.computeEmployeeWage();
+		
+		arr.add(data);
+	
+	}
+	
+	for(Compute data : arr){
+		data.display();	
+	
+	}
+	
+	
+
+	
+	}
+
+
+}
+
+
+
+
+
+
+
+interface  EmpWageBuilder{
+
+	int EMP_PRESENT = 2;
+	int EMP_ABSENT = 0;
+	int EMP_PART_TIME= 1;
+	int FULLTIME = 8;
+	int PARTTIME = 4;
+
+	public void computeEmployeeWage();
+	public void display();	
+
+}
+
+
+
+public class Compute implements EmpWageBuilder{
+
+
+	int empWage = 0;
+	int workingDays = 0;
+	int workingHour = 0;
+	int total = 0 ;
+	int totalWage = 0;
+	String Name = "";
+
+	public  void computeEmployeeWage(){
+
+
+		int  hours=0;
+		for(int i=0 ; ( i <= workingDays && hours <= workingHour ) ; i++){
+
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+
+			switch(empCheck){
+
+				case EMP_PRESENT:
+					total += empWage * FULLTIME;
+					hours += FULLTIME;
+					break;
+				case EMP_PART_TIME:
+					total += empWage * PARTTIME;
+					hours += PARTTIME;
+					break;
+				default :
+
+			}
+
+
+		}
+		totalWage=total;
+		
+	}
+
+	public Compute(String name,int empWage1,int workingDays1, int workingHour1 ){
+	Name = name;
+	empWage = empWage1;
+	workingDays = workingDays1;
+	workingHour = workingHour1;
+
+
+	}
+
+	public void display(){
+	
+	System.out.println("The Total Wage earned by Employee in : "+Name+" is : "+ total);
+	
+	}
+
+
+}
+
